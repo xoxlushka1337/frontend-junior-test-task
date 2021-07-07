987829c2e444243cc5672b7fff6b2b467383eb72
Lev Verbitskiy <simbik@live.ru>
2021-07-07 19:01:15 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const data = require('./data.json');
+
+const task1Result = (animals) => {
+    const result = {};
+    // your code here
+    return result;
+};
+
+console.log(task1Result(data));
+
+const task2Result = (animals) => {
+    const result = 0;
+    // your code here
+    return result;
+};
+
+console.log(task2Result(data));
+
+const task3Result = (animals) => {
+    const result = [];
+    // your code here
+    return result;
+};
+
+console.log(task3Result(data));
+
+const task4Result = (animals) => {
+    const result = [];
+    // your code here
+    return result;
+};
+
+console.log(task4Result(data));
