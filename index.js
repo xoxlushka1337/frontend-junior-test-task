1180956c5942f39a0f6ef3f9e1caf322b7dd3e3a
Lev Verbitskiy <simbik@live.ru>
2022-02-01 11:36:36 +0300

    Improves

--- index.js
+++ index.js
@@ -1,41 +1,50 @@
 const data = require('./data.json');
 
-const task1Result = (animals) => {
+const task11Result = (animals) => {
     const result = {};
     // your code here
     return result;
 };
 
-console.log(task1Result(data));
+console.log(task11Result(data));
 
-const task2Result = (animals) => {
+const task12Result = (animals) => {
     const result = 0;
     // your code here
     return result;
 };
 
-console.log(task2Result(data));
+console.log(task12Result(data));
 
-const task3Result = (animals) => {
+const task13Result = (animals) => {
     const result = [];
     // your code here
     return result;
 };
 
-console.log(task3Result(data));
+console.log(task13Result(data));
 
-const task4Result = (animals) => {
+const task14Result = (animals) => {
     const result = [];
     // your code here
     return result;
 };
 
-console.log(task4Result(data));
+console.log(task14Result(data));
 
-const task5Result = (number, n) => {
+const myPowFunc = (number, n) => {
     const result = number;
     // your code here
     return result;
 };
 
-console.log(task5Result(3, 4));
+console.log(myPowFunc(3, 4));
+
+const myFlatFunc = (inputArray) => {
+    const result = inputArray;
+    // your code here
+    return result;
+};
+
+console.log(myFlatFunc([1, 3, 5, [1, [4,5], 'asdf', [76, [56, [66, 59]]]]]));
+// result 1, 3, 5, 1, 4, 5, 'asdf', 76, 56, 66, 59
