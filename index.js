f42e088f43a7cb44a075062453e613222c97195e
xoxlushka1337 <xoxlushka@gmail.com>
2023-09-20 09:49:09 +0300

    feat: will solve 1 problem

--- index.js
+++ index.js
@@ -1,50 +1,78 @@
-const data = require('./data.json');
+const data = require("./data.json");
 
-const task11Result = (animals) => {
-    const result = {};
-    // your code here
-    return result;
+const task11Result = animals => {
+	const result = {
+		dogs: 0,
+		cats: 0,
+		avgage: 0,
+	};
+
+	let sum = 0;
+	let count = 0;
+
+	// your code here
+
+	for (const animal of animals) {
+		if (animal.type === "cat") {
+			result.cats++;
+		} else if (animal.type === "dog") {
+			result.dogs++;
+		}
+
+		if (Number.isInteger(animal.age)) {
+			sum += animal.age;
+			count++;
+		}
+	}
+
+	if (count > 0) {
+		result.avgage = Math.floor(sum / count);
+	} else {
+		result.avgage = "Нет целочисленных значений в массиве.";
+	}
+
+	return result;
 };
 
 console.log(task11Result(data));
 
-const task12Result = (animals) => {
-    const result = 0;
-    // your code here
-    return result;
-};
+// const task12Result = animals => {
+// 	const result = 0;
+// 	// your code here
+// 	return result;
+// };
 
-console.log(task12Result(data));
+// console.log(task12Result(data));
 
-const task13Result = (animals) => {
-    const result = [];
-    // your code here
-    return result;
-};
+// const task13Result = animals => {
+// 	const result = [];
+// 	// your code here
+// 	return result;
+// };
 
-console.log(task13Result(data));
+// console.log(task13Result(data));
 
-const task14Result = (animals) => {
-    const result = [];
-    // your code here
-    return result;
-};
+// const task14Result = animals => {
+// 	const result = [];
+// 	// your code here
+// 	return result;
+// };
 
-console.log(task14Result(data));
+// console.log(task14Result(data));
 
-const myPowFunc = (number, n) => {
-    const result = number;
-    // your code here
-    return result;
-};
+// const myPowFunc = (number, n) => {
+// 	const result = number;
+// 	// your code here
+// 	return result;
+// };
 
-console.log(myPowFunc(3, 4));
+// console.log(myPowFunc(3, 4));
 
-const myFlatFunc = (inputArray) => {
-    const result = inputArray;
-    // your code here
-    return result;
-};
+// const myFlatFunc = inputArray => {
+// 	const result = inputArray;
+// 	// your code here
+// 	return result;
+// };
 
-console.log(myFlatFunc([1, 3, 5, [1, [4,5], 'asdf', [76, [56, [66, 59]]]]]));
+// console.log(myFlatFunc([1, 3, 5, [1, [4, 5], "asdf", [76, [56, [66, 59]]]]]));
 // result 1, 3, 5, 1, 4, 5, 'asdf', 76, 56, 66, 59
