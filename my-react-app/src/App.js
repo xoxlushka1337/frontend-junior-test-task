5fea063afccdb1213321c52e84059b9c3768621b
xoxlushka1337 <xoxlushka@gmail.com>
2023-09-20 15:44:53 +0300

    feat: solves 3 task

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,26 +1,16 @@
-import React, { useState } from "react";
 import "./App.css";
-import data from "../../data.json";
-import Select from "./Select.jsx";
+import data from "../src/data.json";
+import Select from "./components/Select.jsx";
 
 function App() {
-	const [selectedItem, setSelectedItem] = useState(null);
-
-	const handleSelect = item => {
-		setSelectedItem(item);
+	const handleSelect = selectedItem => {
+		console.log("Выбран элемент:", selectedItem);
 	};
 
 	return (
-		<div className="App">
-			<h1>Select with Autocomplete</h1>
-			<Select data={data} filterKey="name" callback={handleSelect} />
-			{selectedItem && (
-				<div>
-					<h2>Selected Item</h2>
-					<p>Name: {selectedItem.name}</p>
-					{/* Другие свойства объекта selectedItem */}
-				</div>
-			)}
+		<div className="wrapper">
+			<h1>Компонент Select с автодополнением</h1>
+			<Select data={data} onSelect={handleSelect} />
 		</div>
 	);
 }
--- my-react-app/src/components/Select.jsx
+++ my-react-app/src/components/Select.jsx
@@ -1,5 +1,54 @@
-const Select = () => {
-	return <div className="select"></div>;
+import React, { useState } from "react";
+import "../scss/_select.css";
+
+const Select = ({ data, onSelect }) => {
+	const [inputValue, setInputValue] = useState("");
+	const [isOpen, setIsOpen] = useState(false);
+	const [filteredData, setFilteredData] = useState([]);
+
+	const handleInputChange = e => {
+		const value = e.target.value;
+		setInputValue(value);
+		toggleDropdown(value);
+	};
+
+	const toggleDropdown = value => {
+		const filtered = data.filter(item =>
+			item.name.toLowerCase().includes(value.toLowerCase())
+		);
+
+		setFilteredData(filtered);
+		setIsOpen(true);
+	};
+
+	const handleSelectItem = item => {
+		setInputValue(item.name);
+		setIsOpen(false);
+		onSelect(item);
+	};
+
+	return (
+		<div className="select">
+			<input
+				className="select__input"
+				type="text"
+				value={inputValue}
+				onChange={handleInputChange}
+				placeholder="Выберите из списка..."
+				onClick={() => toggleDropdown(inputValue)}
+			/>
+			<ul className={`select__dropdown ${isOpen ? "open" : ""}`}>
+				{filteredData.map(item => (
+					<li
+						className="select__list"
+						key={item.name}
+						onClick={() => handleSelectItem(item)}>
+						{item.name}
+					</li>
+				))}
+			</ul>
+		</div>
+	);
 };
 
 export default Select;
